17daf1873695628804a04142a1db9f637c86c419
the16thpythonist <jonseb1998@gmail.com>
2018-06-07 14:51:57 +0200

    changed the DB_HOST for openshift

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,8 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mariadb');
+// define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
