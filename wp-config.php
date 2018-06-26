1a19c31c8de26746c45a3d06ea22e23e8310219b
the16thpythonist <jonseb1998@gmail.com>
2018-06-26 16:54:21 +0200

    new try

--- wp-config.php
+++ wp-config.php
@@ -25,8 +25,8 @@ define('DB_USER', 'wordpress');
 /** MySQL database password */
 define('DB_PASSWORD', 'wordpress');
 /** MySQL hostname */
-define('DB_HOST', 'mariadb');
 //define('DB_HOST', 'localhost');
+define('DB_HOST', 'mariadb');
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
