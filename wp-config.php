fb2df8db51d15819e928d53c16b388164a3db3e7
the16thpythonist <jonseb1998@gmail.com>
2018-09-11 16:24:16 +0200

    build

--- wp-config.php
+++ wp-config.php
@@ -73,7 +73,7 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 set_time_limit(60 * 60 * 12);
 /* That's all, stop editing! Happy blogging. */
 /** Absolute path to the WordPress directory. */
