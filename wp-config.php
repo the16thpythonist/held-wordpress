94124cdaeea94db47537d8f554dbb0c6a1ce26c1
the16thpythonist <jonseb1998@gmail.com>
2018-12-03 14:15:25 +0100

    init

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,4 @@
 <?php
-define('DB_HOST', 'mariadb');define('WP_HOME', 'http://held.kaas.kit.edu');define('WP_SITEURL', 'http://held.kaas.kit.edu');define('WP_DEBUG', false);
-
 /**
  * The base configuration for WordPress
  *
