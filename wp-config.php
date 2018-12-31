a325de29013fab195b45b907691349275d195e86
the16thpythonist <jonseb1998@gmail.com>
2018-12-31 08:51:40 +0100

    build

--- wp-config.php
+++ wp-config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST', 'mariadb');define('WP_HOME', 'http://held.kaas.kit.edu');define('WP_SITEURL', 'http://held.kaas.kit.edu');define('WP_DEBUG', false);define('DB_USER', 'wordpress');define('DB_PASSWORD', 'wordpress')
+define('DB_HOST', 'mariadb');define('WP_HOME', 'http://held.kaas.kit.edu');define('WP_SITEURL', 'http://held.kaas.kit.edu');define('WP_DEBUG', false);define('DB_USER', 'wordpress');define('DB_PASSWORD', 'wordpress');
 /**
  * The base configuration for WordPress
  *
